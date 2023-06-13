a210776c7d748f40d3bc34bbbf6dcd989002aef4
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-06-13 20:45:19 +0200

    Creating the note

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
+<script setup>
+
+import { ref } from "vue";
+
+const showModal = ref(false)
+
+const newNote = ref("")
+
+
+</script>
+
 <template>
   <main>
-<!--    <div class="overlay">-->
-<!--      <div class="modal">-->
-<!--        <textarea name="note" id="note" cols="30" rows="10"></textarea>-->
-<!--        <button>Add Note</button>-->
-<!--        <button class="close">Close</button>-->
-<!--      </div>-->
-<!--    </div>-->
+    <div v-if="showModal" class="overlay">
+      <div class="modal">
+        <textarea v-model="newNote" name="note" id="note" cols="30" rows="10"></textarea>
+        <button>Add Note</button>
+        <button class="close">Close</button>
+      </div>
+    </div>
     <div class="container">
       <header>
         <h1>Notes</h1>
-        <button>+</button>
+        <button @click="showModal = true">+</button>
       </header>
       <div class="cards-container">
         <div class="card">
